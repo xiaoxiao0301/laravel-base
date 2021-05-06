3feee83b4380f35f1cb374459bfc77a0494f763f
v_xxiaoji <v_xxiaoji@tencent.com>
2021-05-06 16:15:41 +0800

    完成用户注册功能

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -12,6 +12,7 @@
 @include('layout._header')
 <div class="container">
     <div class="offset-md-1 col-md-10">
+        @include('shared._message')
         @yield('content')
         @include('layout._footer')
     </div>
