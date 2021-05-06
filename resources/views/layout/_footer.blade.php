896c71bead2195d050f250bbbfe7b6375d3e4838
v_xxiaoji <v_xxiaoji@tencent.com>
2021-05-06 11:39:24 +0800

    用户注册界面

--- resources/views/layout/_footer.blade.php
+++ resources/views/layout/_footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer">
     <img src="{{ asset('images/user_avatar.png') }}" alt="头像" height="31px" width="31px">
-    <a href="https://note.jixiaoxiao.com" target="_blank">刻意练习，每日精选</a>
+    <a href="https://note.jixiaoxiao.com" target="_blank">刻意练习，每日精进</a>
     <div class="float-right">
         <a href="{{ route('about') }}">关于</a>
     </div>
