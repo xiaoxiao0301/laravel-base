cc9fb4b9f70c7d4eb9ba4d68195d6592fa0e7b1b
v_xxiaoji <v_xxiaoji@tencent.com>
2021-05-06 15:56:11 +0800

    用户注册表单csrf验证

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,6 +8,7 @@
            </div>
            <div class="card-body">
                <form action="{{route('users.store')}}" method="post">
+                   @csrf
                    <div class="form-group">
                        <label for="name">名称:</label>
                        <input type="text" name="name" id="name" class="form-control" value="{{ old('name') }}">
