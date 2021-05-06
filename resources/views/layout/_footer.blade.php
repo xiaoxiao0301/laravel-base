6f5a550730d7e5a8012fe843a62634397d2d5a3e
v_xxiaoji <v_xxiaoji@tencent.com>
2021-05-06 11:31:42 +0800

    命名路由

--- resources/views/layout/_footer.blade.php
+++ resources/views/layout/_footer.blade.php
@@ -2,6 +2,6 @@
     <img src="{{ asset('images/user_avatar.png') }}" alt="头像" height="31px" width="31px">
     <a href="https://note.jixiaoxiao.com" target="_blank">刻意练习，每日精选</a>
     <div class="float-right">
-        <a href="/about">关于</a>
+        <a href="{{ route('about') }}">关于</a>
     </div>
 </footer>
