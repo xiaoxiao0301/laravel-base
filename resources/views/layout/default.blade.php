c24f3792f4dda849c8a4f21e5348f75b650903f1
v_xxiaoji <v_xxiaoji@tencent.com>
2021-05-06 10:23:30 +0800

    初始化样式

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -5,9 +5,19 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title', 'WeiboApp')-Laravel基础操作</title>
+    <title>@yield('title', 'WeiboApp')-Laravel入门教程</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a href="/" class="navbar-brand">Weibo App</a>
+        <ul class="navbar-nav justify-content-end">
+            <li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
+        </ul>
+    </div>
+</nav>
 @yield('content')
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,15 @@
 @extends('layout.default')
 @section('content')
-    <h1>首页</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            Laravel入门教程 <a href="https://learnku.com/docs/laravel/6.x/releases/5121" target="_blank">推荐文档</a>
+        </p>
+        <p>
+            一切，将从这里开始。
+        </p>
+        <p>
+            <a href="#" class="btn btn-lg btn-success">现在注册</a>
+        </p>
+    </div>
 @stop
