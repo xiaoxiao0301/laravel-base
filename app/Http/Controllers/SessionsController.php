04f489c3c181e2e96fabeefdc89247d4338e7d75
v_xxiaoji <v_xxiaoji@tencent.com>
2021-05-07 14:52:42 +0800

    创建会话

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SessionsController extends Controller
+{
+    /**
+     * 登录页面
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    /**
+     * 登录
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+           'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+
+        // Auth::attempt(['email' => $request->email, 'password' => $request->password])
+        if (Auth::attempt($credentials)) {
+            session()->flash('success', "欢迎回来!");
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+            return redirect()->back()->withInput();
+        }
+    }
+
+
+    public function destroy()
+    {
+
+    }
+}
