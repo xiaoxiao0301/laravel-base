da572820f785aead8214a71644ee191419bbb13c
v_xxiaoji <v_xxiaoji@tencent.com>
2021-05-10 10:53:57 +0800

    删除策略区分环境

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -37,6 +37,21 @@ class UserPolicy
      */
     public function destroy(User $currentUser, User $user)
     {
-        return ($currentUser->is_admin && $currentUser->id !== $user->id) ? Response::allow() : Response::deny('您无权限进行此项操作');
+        /**
+         * 这里有一个问题，postgresql中boolean类型存储false的是字符f，存储true的是字符t, 因此在这里需要做一个环境判断
+         */
+        if (getenv('IS_IN_HEROKU')) {
+            // 数据库使用的是pg
+            $userflag = false;
+            if ($currentUser->is_admin == 't') {
+                $userflag = true;
+            }
+            return ($userflag && $currentUser->id !== $user->id) ? Response::allow() : Response::deny('您无权限进行此项操作');
+
+        } else {
+            // 数据库使用的是mysql
+            return ($currentUser->is_admin && $currentUser->id !== $user->id) ? Response::allow() : Response::deny('您无权限进行此项操作');
+        }
+
     }
 }
