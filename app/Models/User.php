f304341c18c9df50812384a76169ec7bca175d30
v_xxiaoji <v_xxiaoji@tencent.com>
2021-05-12 10:58:26 +0800

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -78,7 +78,9 @@ class User extends Authenticatable
      */
     public function feed()
     {
-        return $this->statuses()->orderBy('created_at', 'desc');
+        $userIds = $this->followings->pluck('id')->toArray();
+        array_push($userIds, $this->id);
+        return Status::whereIn('user_id', $userIds)->with('user')->orderBy('created_at', 'desc');
     }
 
     /**
